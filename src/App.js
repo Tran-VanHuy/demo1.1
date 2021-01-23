70fc5eb4838abd651d66d700b7158847173f4ad9
Tran Van Huy <huy1821051049@gmaim.com>
2021-01-24 04:54:07 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+    <div className = "content">
+      <div className = "content_left">
+        <img src={"/Certificate---Shinhan---2_02.jpg"} className = "image_logo" />
+        </div>
+        <div className = "content_right">
+          <div>
+            <div className = "content_img">
+              <img src={"/Certificate---Shinhan---2_07.jpg"} className =  "image_logo_1"/>
+            </div>
+          </div>
+
+          <div  className = "content_header_1">
+            <h1 className = "content_text_2">CERTIFICATE OF COMPLETION</h1>
+          </div>
+
+          <div className = "content_header_2">
+            <p className = "content_text_3">This Certificate is Persented to</p>
+          </div>
+
+          <div className = "content_header_3">
+            <p className = "content_text_4">Vũ Thị Thúy Hằng</p>
+          </div>
+
+          <div className = "content_header_4" >
+            <p className = "content_text_5">Learner ID: K254T</p>
+          </div>
+
+          <div className = "content_header_5">
+            <p className = "content_text_6">In recognition of professional achievement for successfully completing</p>
+          </div>
+
+          <div className = "content_header_6">
+            <h2 className = "content_text_7">Building Aleader Within You</h2>
+          </div>
+
+          <div className = "content_header_7">
+            <p className = "content_text_8">Course ID: K254T</p>
+          </div>
+
+          <div className = "content_header_8">
+            <p className = "content_text_9">September 21-22,2020</p>
+          </div>
+
+          <div className = "content_header_9">
+            <p className = "content_text_10">This is e-certificate applied to e-courses in S-Academy  system</p>
+          </div>
+        </div>
+    </div>
+     
+
+    
+    </div>
+  );
+}
+
+export default App;
